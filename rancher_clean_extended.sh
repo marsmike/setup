379ab5c512b4388cc72431f58bf036174edc01d8
mike <mike@objektarium.de>
2019-04-30 12:09:11 +0000

    Removed -f flag and added deletion of all docker volumes

--- rancher_clean_extended.sh
+++ rancher_clean_extended.sh
@@ -3,8 +3,8 @@
 # docker rm -f $(docker ps -qa)
 # docker rmi -f $(docker images -q)
 # docker volume rm $(docker volume ls -q)
-docker stop -f $(docker ps -qa)
-docker system prune --all --force # better use docker native cli 
+docker stop $(docker ps -qa)
+docker system prune --all --volumes --force # better use docker native cli 
 for mount in $(mount | egrep '/var/lib/kubelet(.*)type (tmpfs|ceph)' | awk '{ print $3 }') /var/lib/kubelet /var/lib/rancher; do umount $mount; done
 cleanupdirs="/etc/ceph /etc/cni /etc/kubernetes /opt/cni /opt/rke /run/secrets/kubernetes.io /run/calico /run/flannel /var/lib/calico /var/lib/etcd /var/lib/cni /var/lib/kubelet /var/lib/rancher/rke/log /var/log/containers /var/log/pods /var/run/calico"
 for dir in $cleanupdirs; do

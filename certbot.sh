25a0c178d5220fa7aa6dd06cfd10a1fc8b4687a1
Mike Müller <mike@objektarium.de>
2019-12-23 09:43:01 +0100

    added certbot.sh

---
+++ certbot.sh
@@ -0,0 +1,7 @@
+sudo add-apt-repository ppa:certbot/certbot
+sudo apt install python-certbot-apache
+
+#sudo ufw allow http
+#sudo ufw allow https
+#sudo certbot --apache -d your_domain -d www.your_domain
+#sudo certbot renew --dry-run

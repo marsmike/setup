e8e0405382588d457dd572793a6f00401dfa5541
root <root@v220200332601111285.ultrasrv.de>
2020-03-24 21:34:02 +0100

    Upgrade to ctop 0.7.3

--- ctop_install.sh
+++ ctop_install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-sudo wget https://github.com/bcicen/ctop/releases/download/v0.7.2/ctop-0.7.2-linux-amd64 -O /usr/local/bin/ctop
+sudo wget https://github.com/bcicen/ctop/releases/download/v0.7.3/ctop-0.7.3-linux-amd64 -O /usr/local/bin/ctop
 sudo chmod +x /usr/local/bin/ctop
 

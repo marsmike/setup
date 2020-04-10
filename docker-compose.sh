ab5a87cedff0040e05b6270f7c8d3854247fb0db
Mike Mueller <mike@objektarium.de>
2020-04-10 09:57:52 +0200

    Update to docker-compose 1.25.4

--- docker-compose.sh
+++ docker-compose.sh
@@ -1,4 +1,4 @@
-sudo curl -L https://github.com/docker/compose/releases/download/1.23.1/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.25.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod 755 /usr/local/bin/docker-compose
 docker-compose --version
 

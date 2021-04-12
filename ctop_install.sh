1fe849bf6d83b0eacfb9e4442e7b9111b58c92c2
Mike Müller <mike@objektarium.de>
2021-04-12 18:06:55 +0200

    Update ctop_install.sh

--- ctop_install.sh
+++ ctop_install.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
-sudo wget https://github.com/bcicen/ctop/releases/download/v0.7.3/ctop-0.7.3-linux-amd64 -O /usr/local/bin/ctop
+sudo wget https://github.com/bcicen/ctop/releases/download/v0.7.5/ctop-0.7.5-linux-amd64 -O /usr/local/bin/ctop
 sudo chmod +x /usr/local/bin/ctop
-

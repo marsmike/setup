2ca0aba1ff5976c3f06ee949d01143f38e15a892
Mike Müller <mike@objektarium.de>
2022-12-13 12:17:36 +0100

    Update basics.sh

--- basics.sh
+++ basics.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 sudo apt update
-sudo apt -y install git curl build-essential docker-compose mc htop vim neovim 
+sudo apt -y install git curl build-essential docker-compose mc htop btop vim neovim 

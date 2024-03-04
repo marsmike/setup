388f639eb226a41b2a3242cc8fd39e661d461fd2
Mike Müller <mike@objektarium.de>
2024-03-04 19:31:31 +0100

    Update basics.sh

--- basics.sh
+++ basics.sh
@@ -3,6 +3,6 @@
 sudo apt update
 sudo apt -y install git apt-transport-https ca-certificates curl build-essential docker-compose htop gh vim wget python3
 
-# Handle with care .. only if AppImage is complaining about missing fuse support
-# => Installs too much packages
-# sudo apt install fuse libfuse2 
+# Run if AppImage is complaining about missing fuse support
+# sudo add-apt-repository universe
+# sudo apt install libfuse2

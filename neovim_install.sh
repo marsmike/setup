d4dc7387a0e38e43f96a038a167a93236f3647ac
Mike Müller <mike@objektarium.de>
2024-03-04 19:30:45 +0100

    Update neovim_install.sh

--- neovim_install.sh
+++ neovim_install.sh
@@ -1,3 +1,7 @@
 curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
 chmod u+x nvim.appimage
 sudo mv nvim.appimage ~/bin/nvim
+
+# FUSE Problem Fix:
+# sudo add-apt-repository universe
+# sudo apt install libfuse2

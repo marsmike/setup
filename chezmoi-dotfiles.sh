ea6c0dd66f1d32ccb7558a40d4cbd3db645b4020
Mike Mueller <mike@objektarium.de>
2024-03-04 17:47:43 +0000

    Fix chezmoi not in the path

--- chezmoi-dotfiles.sh
+++ chezmoi-dotfiles.sh
@@ -1,5 +1,6 @@
 echo "Installing chezmoi and loading my dotfiles..."
 sh -c "$(curl -fsLS get.chezmoi.io)" -- init --apply marsmike
+mkdir -p ~/bin && mv ./bin/chezmoi ~/bin/chezmoi
 echo "Done!"
 
 echo "Loading Tmux plugin manager tpm from git"

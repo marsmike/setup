d8363b4733860ff2c0cafd67f4c8cc5a63b9047d
Mike Müller <mike@objektarium.de>
2024-03-04 17:47:40 +0100

    Update chezmoi.sh

--- chezmoi.sh
+++ chezmoi.sh
@@ -1,1 +1,7 @@
+echo "Installing chezmoi and loading my dotfiles..."
 sh -c "$(curl -fsLS get.chezmoi.io)" -- init --apply marsmike
+echo "Done!"
+
+echo "Loading Tmux plugin manager tpm from git"
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+echo "Done!"

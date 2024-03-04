93ecbf4d30a94b3e1280d117f507eb566be4ad21
Mike Mueller <mike@objektarium.de>
2024-03-04 17:15:51 +0000

    Added anaconda

--- anaconda_install.sh
+++ anaconda_install.sh
@@ -1,3 +1,3 @@
-#Downloading the latest Miniconda installer for Linux. Your architecture may vary.
-wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ~/miniconda.sh
-bash ~/miniconda.sh -b -p $HOME/miniconda
+export INSTALLER_VERSION=Anaconda3-2024.02-1
+curl -L -O https://repo.anaconda.com/archive/$INSTALLER_VERSION-Linux-x86_64.sh
+bash ./$INSTALLER_VERSION-Linux-x86_64.sh

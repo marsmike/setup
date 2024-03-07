c460452233594dd2b5f7a25bc962aa6c7c2230a9
Mike Mueller <mike@objektarium.de>
2024-03-07 20:55:45 +0100

    Fix quickemu install

--- quickemu_install.sh
+++ quickemu_install.sh
@@ -1,1 +1,5 @@
 sudo apt install qemu bash coreutils ovmf grep jq lsb-base procps python3 genisoimage usbutils util-linux sed spice-client-gtk libtss2-tcti-swtpm0 wget xdg-user-dirs zsync unzip
+
+sudo apt-add-repository ppa:flexiondotorg/quickemu
+sudo apt update
+sudo apt install quickemu

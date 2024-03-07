3798f60a0a9a76c97600ad48304b38be44d67236
Mike Mueller <mike@objektarium.de>
2024-03-07 18:51:22 +0100

    Added zsh plugin install

--- ohmyzsh_install.sh
+++ ohmyzsh_install.sh
@@ -1,3 +1,6 @@
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
+git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
+
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
 
